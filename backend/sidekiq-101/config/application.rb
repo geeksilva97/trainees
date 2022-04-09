98243ce5d1035c6cc06dda14cddd5cf0fa852767
geeksilva97 <edigleyssonsilva@gmail.com>
2022-04-09 01:30:33 -0300

    configure sidekiq ui

--- backend/sidekiq-101/config/application.rb
+++ backend/sidekiq-101/config/application.rb
@@ -36,5 +36,7 @@ module Sidekiq101
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.session_store :cookie_store, key: '_interslice_session'
   end
 end
--- backend/sidekiq-101/config/routes.rb
+++ backend/sidekiq-101/config/routes.rb
@@ -1,6 +1,13 @@
+require 'sidekiq/web'
+
+# Configure Sidekiq-specific session middleware
+Sidekiq::Web.use ActionDispatch::Cookies
+Sidekiq::Web.use Rails.application.config.session_store, Rails.application.config.session_options
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
+  mount Sidekiq::Web => "/sidekiq"
 
   get '/sidekiq_demo/get-json', to: 'sidekiq_demo#json'
   resources :sidekiq_demo
